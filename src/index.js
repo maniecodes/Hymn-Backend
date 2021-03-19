21e17cf8deb812171a0e5412dbab1fc37b3d0110
manassehl9 <manassehl9@gmail.com>
2021-03-19 11:28:56 +0000

    configure package.json for production

--- src/index.js
+++ src/index.js
@@ -35,8 +35,7 @@ const server = new ApolloServer({
 });
 
 const app = express();
+app.set("port", process.env.PORT || 4000);
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+app.listen(app.get("port"));
